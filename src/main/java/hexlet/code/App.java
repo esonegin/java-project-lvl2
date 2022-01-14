2b25fc004a6288e5ebf2bf3167c78bb36a7167a5
”Evgeniy <onegin279@gmail.com>
2022-01-14 12:13:51 +0300

    delete unused import

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,7 +6,6 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 @SuppressWarnings("checkstyle:RegexpSingleline")
